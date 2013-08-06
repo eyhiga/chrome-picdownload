1c4ffe0d54aa0fe743da9a78d4b9e95470f14595
Eduardo Higa <eyhiga@gmail.com>
2013-08-05 21:17:00 -0300

    Changing scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,25 @@
 function handleSelectedArea(info, tab) {
-	var url = getURL(info);
-	var selection = document.getSelection();
-	var html = selection.toString();
-	//alert("selection:|" + selection + "|");
-	//var filename = url.substring(url.lastIndexOf('/')+1);
-	//if (chrome.downloads) {
-	//	chrome.downloads.download({ url: url, filename: filename, saveAs: true });
-  	//} else {
-	//	var a = document.createElement('a');
-	//	a.href = url;
-	//	a.download = filename;
-	//	chrome.tabs.create( { 'url' : _anchorDownloader( url, filename ), 'active' : false  } ); // gets around the download limit
-	//}
-	//window.open(url, '_blank');
-	
-	//alert(tab.id);
-	var newTab = chrome.tabs.create({
-		"openerTabId" 	:	tab.id,
-		"url" 			: 	url,
-		"active"		: 	false
-	});
-	//chrome.downloads.download({ url: url, filename: filename, saveAs: true });
+    var url = getURL(info);
+    var selection = document.getSelection();
+    var html = selection.toString();
+    var filename = url.substring(url.lastIndexOf('/')+1);
+
+    if (chrome.downloads) {
+        chrome.downloads.download({ url: url, filename: filename, saveAs: true });
+    } else {
+        var newTab = chrome.tabs.create({
+                "openerTabId" 	:	tab.id,
+                "url" 			: 	url,
+                "active"		: 	false
+                });
+    }
 }
 
 function getURL(info) {
-	var url = info['srcUrl'];
-	url = url.replace("_thumb", "");
+    var url = info['srcUrl'];
+    url = url.replace("_thumb", "");
 
-	return url;
+    return url;
 }
 
 chrome.contextMenus.create({
