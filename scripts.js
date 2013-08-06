40fa1e24bfc5da21b4e1bb8c1c793ebf59a82c50
Eduardo Higa <eyhiga@gmail.com>
2013-08-05 21:21:35 -0300

    Changing scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,53 @@
-function handleSelectedArea(info, tab) {
-    var url = getURL(info);
-    var filename = url.substring(url.lastIndexOf('/')+1);
+function _anchorDownloader(url, filename) {
+    var timeout = 500;
+    return 'javascript:\'<!doctype html><html>'+
+        '<head></head>' +
+        '<script>' +
+        'function initDownload() {'+
+            'var el = document.getElementById("anchor");'+
+                'el.click();' +
+                'setTimeout(function() { window.close(); }, ' + timeout + ');' +
+                '}'+
+                '</script>' +
+                '<body onload="initDownload()">' +
+                '<a id="anchor" href="' + url + '" download="'+ filename + '"></a>'+
+                '</body>' +
+                '</html>\'';
+        };
 
-    if (chrome.downloads) {
-        chrome.downloads.download({ url: url, 
-                filename: filename, saveAs: true });
-    } else {
-        var newTab = chrome.tabs.create({
-                "openerTabId" 	:	tab.id,
-                "url" 			: 	url,
-                "active"		: 	false
-                });
+    function handleSelectedArea(info, tab) {
+        var url = getURL(info);
+        var filename = url.substring(url.lastIndexOf('/')+1);
+
+        if (chrome.downloads) {
+            chrome.downloads.download({ url: url, 
+                    filename: filename, saveAs: true });
+        } else {
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = filename;
+            chrome.tabs.create( { 
+                'url' : _anchorDownloader( url, filename ), 
+                'active' : false  } );
+/*
+            var newTab = chrome.tabs.create({
+                    "openerTabId" 	:	tab.id,
+                    "url" 			: 	url,
+                    "active"		: 	false
+                    });
+*/
+        }
     }
-}
 
-function getURL(info) {
-    var url = info['srcUrl'];
-    url = url.replace("_thumb", "");
+    function getURL(info) {
+        var url = info['srcUrl'];
+        url = url.replace("_thumb", "");
 
-    return url;
-}
+        return url;
+    }
 
-chrome.contextMenus.create({
-        "title"		:   "Open image",
-        "contexts"	:   ["image"],
-        "onclick"	:	handleSelectedArea
-        });
+    chrome.contextMenus.create({
+            "title"		:   "Open image",
+            "contexts"	:   ["image"],
+            "onclick"	:	handleSelectedArea
+            });
